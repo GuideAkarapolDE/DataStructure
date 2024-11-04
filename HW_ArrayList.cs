f9c7c0e37d6798a86711652a3fc74ecacba78b8f
GuideAkarapolDE <6652300320@stu.pim.ac.th>
2024-11-04 14:42:31 +0700

    Add files via upload

---
+++ HW_ArrayList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Windows.Forms;
+using Lists;
+
+namespace HW_ArrayList
+{
+    public partial class ArrayForm : Form
+    {
+        public ArrayForm()
+        {
+            InitializeComponent();
+        }
+
+        List list = new ArrayList(5);
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            string name = FillBox.Text;
+            if (!string.IsNullOrEmpty(name)) // ตรวจสอบว่าชื่อไม่ว่างเปล่า
+            {
+                list.add(name);
+                FillBox.Clear();
+                MessageBox.Show($"คุณได้เพิ่ม {name} เข้ามาในรายการแล้ว");
+            }
+            else
+            {
+                FillBox.Clear();
+                MessageBox.Show("โปรดใส่ข้อความ");
+            }
+            ListLabel.Text = list.size().ToString();
+        }
+        private void FindBtn_Click(object sender, EventArgs e)
+        {
+            string FindName = FillBox.Text;
+            if (list.contains(FindName))
+            {
+                MessageBox.Show($"พบชื่อ {FindName} ในรายการ");
+            }
+            else
+            {
+                MessageBox.Show($"ไม่พบชื่อ {FindName} ในรายการ");
+            }
+            FillBox.Clear();
+        }
+        private void DelBtn_Click(object sender, EventArgs e)
+        {
+            string DelName = FillBox.Text;
+            if (list.contains(DelName))
+            {
+                list.remove(DelName);
+                MessageBox.Show($"ลบชื่อ {DelName} สำเร็จ");
+            }
+            else
+            {
+                MessageBox.Show($"ไม่พบชื่อ {DelName} ในรายการ");
+            }
+            FillBox.Clear();
+            ListLabel.Text = list.size().ToString();
+        }
+        private void FindIndexBtn_Click(object sender, EventArgs e)
+        {
+            int MyIndex;
+            IndexEditBox.Clear();
+
+            if (int.TryParse(FindIndexBox.Text, out MyIndex))
+            {
+
+                MyIndex--; // ทำให้ index เริ่มจาก 1 (มาจาก 1-1 = 0 ซึ่ง 0 จะเท่ากับลำดับที่ 0 ใน index)
+
+                if (MyIndex >= 0 && MyIndex < list.size())
+                {
+                    IndexEditBox.Text = list.get(MyIndex).ToString();
+                    OnIndexEdit();
+                } else
+                {
+                    MessageBox.Show($"ไม่พบรายชื่อลำดับที่ {FindIndexBox.Text} ในรายการ");
+                    OffIndexEdit();
+                }
+            } else
+            {
+                MessageBox.Show($"โปรดใส่ลำดับของรายชื่อให้ถูกต้อง");
+                OffIndexEdit();
+            }
+            //FindIndexBox.Clear();
+        }
+        private void IndexEditBtn_Click(object sender, EventArgs e)
+        {
+            int index = int.Parse(FindIndexBox.Text) - 1; // ได้ index ที่ต้องการแก้ไขมาแล้ว
+            string UpdateData = IndexEditBox.Text;
+
+            
+            list.set(index, UpdateData); // แก้ไขข้อมูลที่ตำแหน่ง index
+            MessageBox.Show($"แก้ไขข้อมูลในลำดับที่ {index + 1} เป็น {UpdateData} สำเร็จ");
+
+            IndexEditBox.Clear();
+            FindIndexBox.Clear();
+        }
+        private void ListShowBtn_Click(object sender, EventArgs e)
+        {
+            ListView.Items.Clear();
+            
+            // วนลูปข้อมูลใน ArrayList และเพิ่มลงใน ListBox
+            for (int i = 0; i < list.size(); i++)
+            {
+                ListView.Items.Add($"{i + 1}. " + list.get(i));
+            }
+        }
+
+        private void OnIndexEdit()
+        {
+            IndexEditBox.Enabled = true;
+            IndexEditBtn.Enabled = true;
+        } // เปิด Box และ Button ของ IndexEdit
+        private void OffIndexEdit()
+        {
+            IndexEditBox.Enabled = false;
+            IndexEditBtn.Enabled = false;
+        } // ปิด Box และ Button ของ IndexEdit
+    }
+}
---
+++ SinglyLinkedList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lists
+{
+    public class SinglyLinkedList : List
+    {
+
+        private class LinkedNode
+        {
+            public object e;
+            public LinkedNode next;
+            public LinkedNode(object e, LinkedNode next) //Constructor
+            {
+                this.e = e;
+                this.next = next;
+            }
+        }
+        //กำหนด first Node โดยให้ค่าเริ่มต้นเป็น null ทั้งซ้าย (e) และขวา (pointer)
+        private LinkedNode first = new LinkedNode(null, null); //สร้าง Header
+        private int SIZE;
+        private LinkedNode nodeAt(int index) //Get node จาก Index ที่ให้
+        {
+            LinkedNode node = first; //first = header ใช้ first.next อาจเป็น null ก็ได้
+            for (int i = -1; i < index; i++) //i = -1 (Header)
+            {
+                node = node.next;
+            }
+            return node;
+        }
+        public void add(int index, object e)
+        {
+            if (index <= SIZE)
+            {
+                LinkedNode node = nodeAt(index - 1); //Node ก่อนหน้าที่จะแทรก (Ant)
+                node.next = new LinkedNode(e, node.next); //node.next = bat --> ทั้งก้อน new = cat
+                SIZE++;
+            }
+        }
+        public void add(object e)
+        {
+            add(SIZE, e);
+        }
+
+        public bool contains(object e)
+        {
+            return indexOf(e) != -1;
+        }
+
+        public object get(int index)
+        {
+            return nodeAt(index).e;
+        }
+
+        public int indexOf(object e)
+        {
+            LinkedNode node = first.next; // เริ่มต้นที่โหนดแรก
+            for (int i = 0; i < SIZE; i++)
+            {
+                if ((e == null && node.e == null) || (e != null && e.Equals(node.e)))
+                {
+                    return i; // หากพบค่า ให้ส่งคืน index
+                }
+                node = node.next; // ไปยังโหนดถัดไป
+            }
+            return -1; // หากไม่พบ ให้คืนค่า -1 (Header)
+        }
+
+        public bool isEmpty()
+        {
+            return first.next == null;
+        }
+        private void removeAfter(LinkedNode node)
+        {
+            if (node.next != null)
+            {
+                node.next = node.next.next; //โยงข้าม
+                SIZE--;
+            }
+
+        }
+        public void remove(int index)
+        {
+            LinkedNode node = nodeAt(index - 1); //ต้องหาตัวก่อนหน้าก่อน
+            removeAfter(node);
+        }
+
+        public void remove(object e)
+        {
+            LinkedNode node = first;
+            while (node.next != null && !node.next.e.Equals(e))
+                node = node.next;
+            removeAfter(node);
+        }
+
+        public void set(int index, object e)
+        {
+            nodeAt(index).e = e;
+        }
+
+        public int size()
+        {
+            return SIZE;
+        }
+    }
+}
